8b65ca966c31d846a89cfe6ac26bff565e430e68
Markus Westerlind <marwes91@gmail.com>
2019-07-21 23:15:42 +0200

    Add lua as a comparison

--- benches/function_call.rs
+++ benches/function_call.rs
@@ -10,21 +10,43 @@ use gluon::vm::thread::{Status, Thread};
 use gluon::{new_vm, Compiler};
 
 // Benchmarks function calls
-fn factorial(b: &mut Bencher) {
+fn factorial(b: &mut Bencher, &input: &i64) {
     let vm = new_vm();
     let text = r#"
     let factorial n =
-        if n < 2
+        if n #Int< 2
         then 1
-        else n * factorial (n - 1)
+        else n #Int* factorial (n #Int- 1)
     factorial
     "#;
-    Compiler::new().load_script(&vm, "factorial", text).unwrap();
-    let mut factorial: FunctionRef<fn(i32) -> i32> = vm.get_global("factorial").unwrap();
+    let (mut factorial, _): (FunctionRef<fn(i64) -> i64>, _) =
+        Compiler::new().run_expr(&vm, "factorial", text).unwrap();
     b.iter(|| {
-        let result = factorial.call(20).unwrap();
+        let result = factorial.call(input).unwrap();
         black_box(result)
     })
+}
+
+fn factorial_lua(b: &mut Bencher, &input: &i64) {
+    let vm = rlua::Lua::new();
+    let text = r#"
+    local function factorial(n)
+        if n < 2 then
+            return 1
+        else
+            return n * factorial(n - 1)
+        end
+    end
+
+    return factorial
+    "#;
+    vm.context(|context| {
+        let factorial: rlua::Function = context.load(text).eval().unwrap();
+        b.iter(|| {
+            let result: i64 = factorial.call((input,)).unwrap();
+            black_box(result)
+        })
+    });
 }
 
 fn factorial_tail_call(b: &mut Bencher) {
@@ -36,8 +58,8 @@ fn factorial_tail_call(b: &mut Bencher) {
         else factorial (a * n) (n - 1)
     factorial 1
     "#;
-    Compiler::new().load_script(&vm, "factorial", text).unwrap();
-    let mut factorial: FunctionRef<fn(i32) -> i32> = vm.get_global("factorial").unwrap();
+    let (mut factorial, _): (FunctionRef<fn(i64) -> i64>, _) =
+        Compiler::new().run_expr(&vm, "factorial", text).unwrap();
     b.iter(|| {
         let result = factorial.call(20).unwrap();
         black_box(result)
@@ -71,17 +93,23 @@ fn gluon_rust_boundary_overhead(b: &mut Bencher) {
     "#;
     Compiler::new().load_script(&vm, "test", text).unwrap();
 
-    let mut test: FunctionRef<fn(i32, Primitive<fn(i32)>) -> ()> = vm.get_global("test").unwrap();
+    let mut test: FunctionRef<fn(i64, Primitive<fn(i64)>) -> ()> = vm.get_global("test").unwrap();
     b.iter(|| {
         let result = test
-            .call(1000, primitive::<fn(i32)>("test_fn", test_fn))
+            .call(1000, primitive::<fn(i64)>("test_fn", test_fn))
             .unwrap();
         black_box(result)
     })
 }
 
 fn function_call_benchmark(c: &mut Criterion) {
-    c.bench_function("factorial", factorial);
+    env_logger::init();
+
+    c.bench(
+        "factorial",
+        criterion::ParameterizedBenchmark::new("gluon", factorial, vec![1, 10, 20])
+            .with_function("lua", factorial_lua),
+    );
     c.bench_function("factorial tail call", factorial_tail_call);
     c.bench_function("gluon rust boundary overhead", gluon_rust_boundary_overhead);
 }
